2f4550218f7bb2c97a4a1e1e5ad0e41434674e35
AksTraL <146377159+AksTraL@users.noreply.github.com>
2023-10-04 07:31:04 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,16 @@
+# For Loop Example
+
+fruits= ["apple", "banana", "mango" , "orange"]
+for color in fruits:
+  print(color)
+  for x in color:
+    print(x)
+
+for k in range(3,31,3):
+  print(k)
+
+Name= "Akshat"
+for i in Name:
+  print(i)
+  if (i =="k"):
+    print("This is so unique")
